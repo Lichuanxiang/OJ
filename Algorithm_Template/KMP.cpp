1c74e926b3983c4e26269bf68f09cc0759a3094f
lichuanxiang <chuanxiang.lee@gmail.com>
2018-08-08 14:18:12 +0800

    Update KMP algorithm

--- Algorithm_Template/KMP.cpp
+++ Algorithm_Template/KMP.cpp
@@ -1,5 +1,24 @@
 //由Knuth,Morris,Pratt共同发现
 //时间复杂度O(n+m),穷举为O(nm)
+/*
+	考察目标字符串ptr： 
+	ababaca 
+	这里要计算一个长度为m的转移函数next。
+
+	next数组的含义就是一个固定字符串的最长前缀和最长后缀相同的长度。
+
+	比如：abcjkdabc，那么这个数组的最长前缀和最长后缀相同必然是abc。 
+	cbcbc，最长前缀和最长后缀相同是cbc。 
+	abcbc，最长前缀和最长后缀相同是不存在的。
+
+	**注意最长前缀：是说以第一个字符开始，但是不包含最后一个字符。 
+	比如aaaa相同的最长前缀和最长后缀是aaa。** 
+	对于目标字符串ptr，ababaca，长度是7，所以next[0]，...，next[6]分别计算的是:
+	a，ab，aba，abab，ababa，ababac，ababaca的相同的最长前缀和最长后缀的长度。
+	由于a，ab，aba，abab，ababa，ababac，ababaca的相同的最长前缀和最长后缀是
+	“”，“”，“a”，“ab”，“aba”，“”，“a”,所以next数组的值是[-1,-1,0,1,2,-1,0]，
+	这里-1表示不存在，0表示存在长度为1，2表示存在长度为3。这是为了和代码相对应。
+*/
 #include <cstdio>
 #include <cstring>
 
@@ -20,6 +39,7 @@ void getNext(char s[], int* next, int len){
 	printf("\n");
 }
 
+//返回模式串pattern匹配的次数
 int KMP(char* text, char* pattern){
 	int n = strlen(text), m = strlen(pattern);
 	int next[m];
